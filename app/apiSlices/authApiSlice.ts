33c45c43067537e269079d88ca211432ae80ffc8
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-04-23 16:41:26 +0330

    refactor: refresh token function

--- app/apiSlices/authApiSlice.ts
+++ app/apiSlices/authApiSlice.ts
@@ -1,55 +1,57 @@
-import { apiSlice } from "@/app/config/api-config/apiSlice"
-import { logOut, setCredentials } from "./authSlice"
+import { apiSlice } from "@/app/config/api-config/apiSlice";
+import { logOut, setCredentials } from "./authSlice";
 
 /* This API Slice is for:
-  * User Login
-  * User Logout
-  * Updating Refresh Token
-*/
+ * User Login
+ * User Logout
+ * Updating Refresh Token
+ */
 
 export const authApiSlice = apiSlice.injectEndpoints({
-  endpoints: builder => ({
-
+  endpoints: (builder) => ({
     login: builder.mutation({
-      query: credentials => ({
-        url: '/auth',
-        method: 'POST',
-        body: {...credentials}
-      })
+      query: (credentials) => ({
+        url: "/auth",
+        method: "POST",
+        body: { ...credentials },
+      }),
     }),
 
     sendLogout: builder.mutation({
       query: () => ({
-        url: '/auth/logout',
-        method: 'POST'
+        url: "/auth/logout",
+        method: "POST",
       }),
 
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
-          await queryFulfilled
-          dispatch(logOut(undefined))
-          dispatch(apiSlice.util.resetApiState())
-        } catch (error) { console.log("ERROR", error) }
-      }
+          await queryFulfilled;
+          dispatch(logOut(undefined));
+          dispatch(apiSlice.util.resetApiState());
+        } catch (error) {
+          console.log("ERROR", error);
+        }
+      },
     }),
 
     refresh: builder.mutation({
       query: () => ({
-        url: '/auth/refresh',
-        method: 'GET'
+        url: "/auth/refresh",
+        method: "GET",
       }),
 
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
-          const { data } = await queryFulfilled
-          const { accessToken } = data
-          dispatch(setCredentials({ accessToken }))
+          const { data } = await queryFulfilled;
+          const { accessToken } = data;
+          dispatch(setCredentials({ accessToken }));
         } catch (err) {
-          console.log(err)
+          console.log("errResresh", err);
         }
-      }
-    })
-  })
-})
+      },
+    }),
+  }),
+});
 
-export const { useLoginMutation, useSendLogoutMutation, useRefreshMutation } = authApiSlice
+export const { useLoginMutation, useSendLogoutMutation, useRefreshMutation } =
+  authApiSlice;
