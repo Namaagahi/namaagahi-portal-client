7b21e010f267c8f2e75a9642dd7e896cb6ba9681
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-05-05 12:37:02 +0330

    debug: redirect to dashboard and login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-"use client";
 import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-const Login = dynamic(() => import("./features/login/Login"), { ssr: false });
+import Login from "./features/login/Login";
 
 export default function Home() {
-  const { push } = useRouter();
-  const accessToken =
-    typeof window !== "undefined" && window.localStorage.getItem("CC_Token");
-  if (accessToken) push("/dashboard");
-  return <>({!accessToken && <Login />})</>;
+  return <Login />;
 }
