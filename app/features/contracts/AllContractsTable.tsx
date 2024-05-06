141e9f7d1fb0b6667b9ef85dbbe1a2f531607cb7
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-05-06 10:23:07 +0330

    refactor: fetch data in first render by delete useMemo

--- app/features/contracts/AllContractsTable.tsx
+++ app/features/contracts/AllContractsTable.tsx
@@ -44,158 +44,154 @@ const AllContractsTable: FC<IAllContractsTableProps> = ({
     (state) => selectAllFinalCustomers(state) as FinalCustomerObject[]
   );
 
-  const columns = useMemo<ColumnDef<PlanObject>[]>(() => {
-    return [
-      {
-        header: "جدول قراردادها",
-        columns: [
-          {
-            accessorKey: "_id",
-            accessorFn: (row) => row.id,
-            id: "_id",
-            cell: (info) => null,
-            header: () => null,
+  const columns: ColumnDef<PlanObject>[] = [
+    {
+      header: "جدول قراردادها",
+      columns: [
+        {
+          accessorKey: "_id",
+          accessorFn: (row) => row.id,
+          id: "_id",
+          cell: (info) => null,
+          header: () => null,
+        },
+        {
+          accessorFn: (row) => row.initialCustomerId,
+          id: "مشتری",
+          cell: (info) => {
+            const customer: InitialCustomerObject | undefined =
+              allInitialCustomers.find(
+                (customer: InitialCustomerObject) =>
+                  customer.id === info.getValue()
+              );
+            return <div>{customer?.name}</div>;
           },
-          {
-            accessorFn: (row) => row.initialCustomerId,
-            id: "مشتری",
-            cell: (info) => {
-              const customer: InitialCustomerObject | undefined =
-                allInitialCustomers.find(
-                  (customer: InitialCustomerObject) =>
-                    customer.id === info.getValue()
-                );
-              return <div>{customer?.name}</div>;
-            },
-            header: () => <span>نام مشتری</span>,
-            enableColumnFilter: false,
-          },
-          {
-            accessorFn: (row) => row.brand,
-            id: "برند",
-            cell: (info) => info.getValue(),
-            header: () => <span>برند</span>,
-          },
-          {
-            accessorFn: (row) => row.structures,
-            id: "تاریخ شروع",
-            header: () => <span>تاریخ شروع</span>,
-            enableColumnFilter: false,
-            cell: (info) => {
-              const number = info
-                .getValue()
-                .map((x: any) => x.duration.sellStart);
-              const startedAt = Math.min(...number);
+          header: () => <span>نام مشتری</span>,
+          enableColumnFilter: false,
+        },
+        {
+          accessorFn: (row) => row.brand,
+          id: "برند",
+          cell: (info) => info.getValue(),
+          header: () => <span>برند</span>,
+        },
+        {
+          accessorFn: (row) => row.structures,
+          id: "تاریخ شروع",
+          header: () => <span>تاریخ شروع</span>,
+          enableColumnFilter: false,
+          cell: (info) => {
+            const number = info
+              .getValue()
+              .map((x: any) => x.duration.sellStart);
+            const startedAt = Math.min(...number);
 
-              return <p>{moment.unix(startedAt).format("jYYYY-jM-jD")}</p>;
-            },
+            return <p>{moment.unix(startedAt).format("jYYYY-jM-jD")}</p>;
           },
-          {
-            accessorFn: (row) => row.structures,
-            id: "تاریخ پایان",
-            header: () => <span>تاریخ پایان</span>,
-            enableColumnFilter: false,
-            cell: (info) => {
-              const number = info
-                .getValue()
-                .map((x: any) => x.duration.sellEnd);
-              const finishedAt = Math.max(...number);
-              return <p>{moment.unix(finishedAt).format("jYYYY-jM-jD")}</p>;
-            },
+        },
+        {
+          accessorFn: (row) => row.structures,
+          id: "تاریخ پایان",
+          header: () => <span>تاریخ پایان</span>,
+          enableColumnFilter: false,
+          cell: (info) => {
+            const number = info.getValue().map((x: any) => x.duration.sellEnd);
+            const finishedAt = Math.max(...number);
+            return <p>{moment.unix(finishedAt).format("jYYYY-jM-jD")}</p>;
           },
-          {
-            accessorFn: (row) => row.structures,
-            id: "تعداد سازه",
-            cell: (info) => info.getValue().length,
-            header: () => <span>تعداد سازه</span>,
-            enableColumnFilter: false,
+        },
+        {
+          accessorFn: (row) => row.structures,
+          id: "تعداد سازه",
+          cell: (info) => info.getValue().length,
+          header: () => <span>تعداد سازه</span>,
+          enableColumnFilter: false,
+        },
+        {
+          accessorFn: (row) => row?.structures,
+          id: "جمع دوره",
+          cell: (info) => {
+            const totalCost = info
+              .getValue()
+              .map((x: any) => x.totalPeriodCost);
+            return (
+              <p>
+                {formatNumber(
+                  totalCost.reduce((a: number, b: number) => a + b),
+                  ","
+                )}
+              </p>
+            );
           },
-          {
-            accessorFn: (row) => row?.structures,
-            id: "جمع دوره",
-            cell: (info) => {
-              const totalCost = info
-                .getValue()
-                .map((x: any) => x.totalPeriodCost);
-              return (
-                <p>
-                  {formatNumber(
-                    totalCost.reduce((a: number, b: number) => a + b),
-                    ","
-                  )}
-                </p>
-              );
-            },
-            header: () => <span>جمع دوره</span>,
+          header: () => <span>جمع دوره</span>,
+        },
+        {
+          accessorFn: (row) => row,
+          id: "فاکتور رسمی",
+          cell: (info) => {
+            const finalCustomer = allFinalCustomers.find(
+              (finalCustomer: FinalCustomerObject) =>
+                finalCustomer?.finalCustomerId ===
+                info.getValue().finalCustomerId
+            );
+            return (
+              <p>
+                {finalCustomer?.contractType === "official"
+                  ? "رسمی"
+                  : "غیر رسمی"}
+              </p>
+            );
           },
-          {
-            accessorFn: (row) => row,
-            id: "فاکتور رسمی",
-            cell: (info) => {
-              const finalCustomer = allFinalCustomers.find(
-                (finalCustomer: FinalCustomerObject) =>
-                  finalCustomer?.finalCustomerId ===
-                  info.getValue().finalCustomerId
-              );
-              return (
-                <p>
-                  {finalCustomer?.contractType === "official"
-                    ? "رسمی"
-                    : "غیر رسمی"}
-                </p>
-              );
-            },
-            header: () => <span>فاکتور رسمی</span>,
+          header: () => <span>فاکتور رسمی</span>,
+        },
+        {
+          accessorFn: (row) => row,
+          id: "حقیقی یا حقوقی",
+          cell: (info) => {
+            const finalCustomer = allFinalCustomers.find(
+              (finalCustomer: FinalCustomerObject) =>
+                finalCustomer?.finalCustomerId ===
+                info.getValue().finalCustomerId
+            );
+            return (
+              <p>
+                {finalCustomer?.customerType === "legal" ? "حقوقی" : "حقیقی"}
+              </p>
+            );
           },
-          {
-            accessorFn: (row) => row,
-            id: "حقیقی یا حقوقی",
-            cell: (info) => {
-              const finalCustomer = allFinalCustomers.find(
-                (finalCustomer: FinalCustomerObject) =>
-                  finalCustomer?.finalCustomerId ===
-                  info.getValue().finalCustomerId
-              );
-              return (
-                <p>
-                  {finalCustomer?.customerType === "legal" ? "حقوقی" : "حقیقی"}
-                </p>
-              );
-            },
-            header: () => <span>مشتری(حقیقی یا حقوقی)</span>,
+          header: () => <span>مشتری(حقیقی یا حقوقی)</span>,
+        },
+        {
+          accessorFn: (row) => row,
+          id: "نام مدیر یا نماینده",
+          cell: (info) => {
+            const finalCustomer = allFinalCustomers.find(
+              (finalCustomer: FinalCustomerObject) =>
+                finalCustomer?.finalCustomerId ===
+                info.getValue().finalCustomerId
+            );
+            return <p>{finalCustomer?.agent[0].agentName}</p>;
           },
-          {
-            accessorFn: (row) => row,
-            id: "نام مدیر یا نماینده",
-            cell: (info) => {
-              const finalCustomer = allFinalCustomers.find(
-                (finalCustomer: FinalCustomerObject) =>
-                  finalCustomer?.finalCustomerId ===
-                  info.getValue().finalCustomerId
-              );
-              return <p>{finalCustomer?.agent[0].agentName}</p>;
-            },
-            header: () => <span>نام مدیر یا نماینده</span>,
-            enableColumnFilter: false,
+          header: () => <span>نام مدیر یا نماینده</span>,
+          enableColumnFilter: false,
+        },
+        {
+          id: "پیش فاکتور",
+          header: () => <span>پیش فاکتور</span>,
+          cell: (info) => {
+            const row = info.row.original;
+            return (
+              <Link href={`/dashboard/billboard/contracts/${row.id}`}>
+                <div className="flex justify-center text-xl text-red-600 dark:text-red-300 transition-all dark:hover:text-gray-300 hover:text-gray-500 cursor-pointer">
+                  <FaFilePdf />
+                </div>
+              </Link>
+            );
           },
-          {
-            id: "پیش فاکتور",
-            header: () => <span>پیش فاکتور</span>,
-            cell: (info) => {
-              const row = info.row.original;
-              return (
-                <Link href={`/dashboard/billboard/contracts/${row.id}`}>
-                  <div className="flex justify-center text-xl text-red-600 dark:text-red-300 transition-all dark:hover:text-gray-300 hover:text-gray-500 cursor-pointer">
-                    <FaFilePdf />
-                  </div>
-                </Link>
-              );
-            },
-          },
-        ],
-      },
-    ];
-  }, []);
+        },
+      ],
+    },
+  ];
 
   return <TableComponent columns={columns} data={data} />;
 };
