f52e343c2dd492cda04b58dccd06528fea9d7f91
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-05-26 12:15:43 +0330

    feature: implement asset component

---
+++ app/features/itAssets/Assets.tsx
@@ -0,0 +1,59 @@
+import {
+  selectAllAssets,
+  useGetITassetsQuery,
+} from "@/app/apiSlices/itAssetsApiSlice";
+import { AssetObject } from "@/app/lib/interfaces";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Loading from "../loading/Loading";
+import PageTitle from "@/app/components/main/PageTitle";
+import SearchContainer from "@/app/components/main/SearchContainer";
+import Button from "@/app/components/main/Button";
+import ScrollContainer from "@/app/components/main/ScrollContainer";
+import AllAssetsTable from "./AllAssetsTable";
+import useAuth from "@/app/hooks/useAuth";
+
+const Assets = () => {
+  const [data, setData] = useState<AssetObject[]>([]);
+
+  const { isLoading, isError } = useGetITassetsQuery(undefined, {
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+    refetchOnReconnect: true,
+    // pollingInterval: 10000,
+  });
+
+  const allAssets: AssetObject[] = useSelector(
+    (state) => selectAllAssets(state) as AssetObject[]
+  );
+
+  useEffect(() => {
+    setData(allAssets);
+  }, [allAssets]);
+
+  if (isLoading || !allAssets[0]) return <Loading />;
+
+  if (isError)
+    return (
+      <div className="flex flex-col justify-center items-center min-h-screen gap-3">
+        <p className="text-xl">هیچ دارایی وجود ندارد</p>
+      </div>
+    );
+
+  return (
+    <>
+      <PageTitle name={"دارایی های IT"} />
+      <div className="flex items-center justify-between gap-3">
+        <SearchContainer />
+        <Button
+          onClickHandler={() => console.log("clicked")}
+          title="ثبت دارایی  جدید"
+        />
+      </div>
+      <AllAssetsTable data={data} />
+      <ScrollContainer />
+    </>
+  );
+};
+
+export default Assets;
