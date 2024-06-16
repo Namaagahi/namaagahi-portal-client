f3506456f462f9cd6da639f92cec58e23b4c9f2e
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-06-16 10:14:32 +0330

    refactor: change boxes creator name

--- app/features/boxes/BoxItem.tsx
+++ app/features/boxes/BoxItem.tsx
@@ -1,31 +1,33 @@
-import { selectBoxById } from "../../apiSlices/boxesApiSlice"
-import {  BoxObject } from "@/app/lib/interfaces"
-import useAuth from "@/app/hooks/useAuth"
-import { useSelector } from "react-redux"
-import dynamic from 'next/dynamic'
-const ListItem = dynamic(
-  () => import('@/app/components/main/ListItem'),
-  { ssr: false }
-)
+import { selectBoxById } from "../../apiSlices/boxesApiSlice";
+import { BoxObject, UserObject } from "@/app/lib/interfaces";
+import useAuth from "@/app/hooks/useAuth";
+import { useSelector } from "react-redux";
+import dynamic from "next/dynamic";
+import { selectAllUsers } from "@/app/apiSlices/usersApiSlice";
+const ListItem = dynamic(() => import("@/app/components/main/ListItem"), {
+  ssr: false,
+});
 
 type Props = {
-  boxId: string
-  index: number
-  page: string
-}
+  boxId: string;
+  index: number;
+  page: string;
+};
 
 const BoxItem = (props: Props) => {
+  const { boxId, index, page } = props;
+  const { id } = useAuth();
 
-  const {
-    boxId,
-    index,
-    page
-  } = props
+  const box: BoxObject = useSelector(
+    (state) => selectBoxById(state, boxId) as BoxObject
+  );
 
-  const { id } = useAuth()
-  const box: BoxObject = useSelector(state => selectBoxById(state, boxId) as BoxObject)
-  return (
-  page === 'my' && box.userId === id ?
+  const allUsers: UserObject[] = useSelector(
+    (state) => selectAllUsers(state) as UserObject[]
+  );
+  const user = allUsers.find((x) => x.username === box.username);
+
+  return page === "my" && box.userId === id ? (
     <ListItem
       number={index}
       param={boxId}
@@ -34,14 +36,14 @@ const BoxItem = (props: Props) => {
       endDate={box.duration.endDate}
       diff={box.duration.diff}
       titles={{
-        'نام باکس': box.name,
-        'نوع باکس': box.mark.name,
-        'کاربر ایجاد کننده': box.username,
-        'کد پروژه': box.mark.markOptions?.projectNumber,
-        'برند': box.mark.markOptions?.brand
+        "نام باکس": box.name,
+        "نوع باکس": box.mark.name,
+        "کاربر ایجاد کننده": box.username,
+        "کد پروژه": box.mark.markOptions?.projectNumber,
+        برند: box.mark.markOptions?.brand,
       }}
     />
-    :
+  ) : (
     <ListItem
       number={index}
       param={boxId}
@@ -50,14 +52,14 @@ const BoxItem = (props: Props) => {
       endDate={box.duration.endDate}
       diff={box.duration.diff}
       titles={{
-        'نام باکس': box.name,
-        'نوع باکس': box.mark.name,
-        'کاربر ایجاد کننده': box.username,
-        'کد پروژه': box.mark.markOptions?.projectNumber,
-        'برند': box.mark.markOptions?.brand
+        "نام باکس": box.name,
+        "نوع باکس": box.mark.name,
+        "کاربر ایجاد کننده": user?.name,
+        "کد پروژه": box.mark.markOptions?.projectNumber,
+        برند: box.mark.markOptions?.brand,
       }}
     />
-  )
-}
+  );
+};
 
-export default BoxItem
+export default BoxItem;
