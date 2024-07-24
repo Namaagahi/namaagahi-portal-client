e18bd0fce8c9a4adbec2b43bac6ac6b87b10c9d1
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-07-24 10:00:40 +0330

    fix: try to fix token expire issue

--- app/config/state-config/SocketContext.tsx
+++ app/config/state-config/SocketContext.tsx
@@ -1,61 +1,64 @@
-"use client"
-import React, { createContext, useContext, useState, useEffect } from 'react'
-import { io, Socket } from 'socket.io-client'
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
 
 interface SocketContextType {
-  socket: Socket | null
-  setupSocket: () => void
+  socket: Socket | null;
+  setupSocket: () => void;
 }
 
-const SocketContext = createContext<SocketContextType | undefined>(undefined)
+const SocketContext = createContext<SocketContextType | undefined>(undefined);
 
 interface SocketProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
 
-  const [socket, setSocket] = useState<Socket | null>(null)
-
-  const token = typeof window !== 'undefined' && window.localStorage && window.localStorage.getItem("CC_Token")
+  const token =
+    typeof window !== "undefined" &&
+    window.localStorage &&
+    window.localStorage.getItem("CC_Token");
   const setupSocket = () => {
-    if (typeof window !== 'undefined' && window.localStorage) {
+    if (typeof window !== "undefined" && window.localStorage) {
       if (token) {
         const newSocket = io(process.env.SERVER!, {
           query: {
             token,
           },
-        })
+        });
 
         newSocket.on("disconnect", () => {
-          setSocket(null)
-          setTimeout(setupSocket, 3000)
+          setSocket(null);
+          setTimeout(setupSocket, 3000);
           // toast.warn("سوکت قطع شد")
-        })
+        });
 
         newSocket.on("connect", () => {
           // toast.info("سوکت وصل شد")
-        })
+        });
 
-        setSocket(newSocket)
+        setSocket(newSocket);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    setupSocket()
+    setupSocket();
     //eslint-disable-next-line
-  }, [token])
+  }, [token]);
 
   return (
     <SocketContext.Provider value={{ socket, setupSocket }}>
       {children}
     </SocketContext.Provider>
-  )
-}
+  );
+};
 
 export const useSocket = () => {
-  const context = useContext(SocketContext)
-  if (!context) throw new Error('useSocket must be used within a SocketProvider')
-  return context
-}
+  const context = useContext(SocketContext);
+  if (!context)
+    throw new Error("useSocket must be used within a SocketProvider");
+  return context;
+};
