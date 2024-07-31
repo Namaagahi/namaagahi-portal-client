46a0f128d34abdb10c129aa39cca90a8614a442a
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-07-31 08:47:23 +0330

    refactor: change summarybox logic in regular mode

--- app/features/plans/SummaryBox.tsx
+++ app/features/plans/SummaryBox.tsx
@@ -1,71 +1,77 @@
-import { AddPlanForm, CombinedStructure, EditPlanForm } from '@/app/lib/interfaces'
-import { UseFormSetValue } from 'react-hook-form'
-import Loading from '../loading/Loading'
-import { useEffect } from 'react'
+import {
+  AddPlanForm,
+  BoxObject,
+  CombinedStructure,
+  EditPlanForm,
+} from "@/app/lib/interfaces";
+import { UseFormSetValue } from "react-hook-form";
+import Loading from "../loading/Loading";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectAllBoxes } from "@/app/apiSlices/boxesApiSlice";
 
 type Props = {
-    structure: CombinedStructure
-    selectedStructure: CombinedStructure
-    setValue: UseFormSetValue<EditPlanForm> | UseFormSetValue<AddPlanForm>
-    fieldIndex: number
-}
+  structure: CombinedStructure;
+  selectedStructure: CombinedStructure;
+  setValue: UseFormSetValue<EditPlanForm> | UseFormSetValue<AddPlanForm>;
+  fieldIndex: number;
+  structureId?: string;
+};
 
 const SummaryBox = (props: Props) => {
+  const { structure, selectedStructure, setValue, fieldIndex, structureId } =
+    props;
 
-    const {
-        structure,
-        selectedStructure,
-        setValue,
-        fieldIndex
-    } = props
+  useEffect(() => {
+    setValue(`structures.${fieldIndex}.structureRecord`, selectedStructure);
+  }, []);
 
-    useEffect(() => {
-        setValue(`structures.${fieldIndex}.structureRecord`, selectedStructure)
-    }, [])
+  const allBoxes: BoxObject[] = useSelector(
+    (state) => selectAllBoxes(state) as BoxObject[]
+  );
 
-    if(!structure) return <Loading />
+  const selectedStructureFromBox = allBoxes
+    .map((box) => box.structures)
+    .flat()
+    .filter((str) => str.structureId === structureId);
+
+  if (!structure) return <Loading />;
   return (
     <>
-        <div className='flex gap-3'>
-            <label
-                htmlFor="typeName"
-                className='text-[#767676] dark:text-white font-bold'
-            >
-                مسیر
-            </label>
+      <div className="flex gap-3">
+        <label
+          htmlFor="typeName"
+          className="text-[#767676] dark:text-white font-bold"
+        >
+          مسیر
+        </label>
 
-            <p>
-                {structure?.location?.path}
-            </p>
-        </div>
+        <p>{structure?.location?.path}</p>
+      </div>
 
-        <div className='flex gap-3'>
-            <label
-                htmlFor="typeName"
-                className='text-[#767676] dark:text-white font-bold'
-            >
-                نشانی
-            </label>
+      <div className="flex gap-3">
+        <label
+          htmlFor="typeName"
+          className="text-[#767676] dark:text-white font-bold"
+        >
+          نشانی
+        </label>
 
-            <p>
-                {structure?.location?.address}
-            </p>
-        </div>
+        <p>{structure?.location?.address}</p>
+      </div>
 
-        <div className='flex gap-3'>
-            <label
-                htmlFor="typeName"
-                className='text-[#767676] dark:text-white font-bold'
-            >
-                مساحت
-            </label>
+      <div className="flex gap-3">
+        <label
+          htmlFor="typeName"
+          className="text-[#767676] dark:text-white font-bold"
+        >
+          مساحت
+        </label>
 
-            <p>
-                {structure?.marks?.markOptions?.printSize}
-            </p>
-        </div>
+        <p>{selectedStructureFromBox[0]?.marks?.markOptions?.printSize}</p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SummaryBox
+export default SummaryBox;
