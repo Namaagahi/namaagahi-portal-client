610a44e027dd6d2ad25282cf2008c3887ada91a1
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-10-30 11:25:54 +0330

    feature: add available structures to chooseStructure modal

--- app/components/modals/ChooseStructureModal.tsx
+++ app/components/modals/ChooseStructureModal.tsx
@@ -1,11 +1,29 @@
 "use client";
-import { StructureObject } from "@/app/lib/interfaces";
-import React, { useState } from "react";
+import {
+  CombinedStructure,
+  PlanObject,
+  StructureObject,
+} from "@/app/lib/interfaces";
+import React, { useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import DatePicker from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import DateObject from "react-date-object";
+import { calculateAvailableDates } from "@/app/utilities/calculateAvailableDates";
+import {
+  selectAllPlans,
+  useGetAllPlansQuery,
+} from "@/app/apiSlices/plansApiSlice";
+import {
+  selectAllStructures,
+  useGetStructuresQuery,
+} from "@/app/apiSlices/structuresApiSlice";
+import { useSelector } from "react-redux";
 
 type Props = {
   handleModal: () => void;
-  data: StructureObject[];
+  data: CombinedStructure[];
   fieldIndex: number;
   setValue: any;
   handleThisStructuresChange: (index: number, val: string) => void;
@@ -26,14 +44,54 @@ const ChooseStructureModal = (props: Props) => {
 
   const [searchText, setSearchText] = useState<string>("");
   const [searchResults, setSearchResults] = useState<StructureObject[]>([]);
+  const [mainData, setMainData] = useState<CombinedStructure[]>([]);
   const [selectedItem, setSelectedItem] = useState<StructureObject | null>(
     null
   );
   const [isAlreadySelected, setIsAlreadySelected] = useState(false);
+  const [empty, setEmpty] = useState<boolean>(false);
+  const [startDate, setStartDate] = useState<number>(
+    new Date().getTime() / 1000
+  );
+  const [endDate, setEndDate] = useState<number>(new Date().getTime() / 1000);
+
+  const { isLoading, isError } = useGetAllPlansQuery(undefined, {
+    refetchOnFocus: false,
+    refetchOnMountOrArgChange: false,
+  });
+
+  const allPlans: PlanObject[] = useSelector(
+    (state) => selectAllPlans(state) as PlanObject[]
+  );
+
+  useGetStructuresQuery(undefined, {
+    refetchOnFocus: false,
+    refetchOnMountOrArgChange: false,
+  });
+  const allStructures: StructureObject[] = useSelector(
+    (state) => selectAllStructures(state) as StructureObject[]
+  );
+
+  const handleEmpty = () => {
+    const availableStructures = calculateAvailableDates(
+      allStructures,
+      allPlans,
+      data,
+      startDate,
+      endDate
+    );
+    setMainData(
+      empty ? data.filter((x) => availableStructures.has(x.name)) : data
+    );
+  };
+
+  useEffect(() => {
+    handleEmpty();
+  }, [empty]);
 
   const performSearch = (value: string) => {
     const searchText = value.toLowerCase();
-    const filteredResults = data.filter((item: StructureObject) => {
+    const filteredResults = mainData.filter((item: StructureObject) => {
       return (
         item.name?.toLowerCase().includes(searchText) ||
         item.location?.address.toLowerCase().includes(searchText) ||
@@ -90,20 +148,67 @@ const ChooseStructureModal = (props: Props) => {
               </small>
             )}
             <div className="py-7 border-[1px] border-x-transparent border-y-[#FA9E93]">
-              <input
-                type="text"
-                placeholder="جستجو"
-                className="formInput2"
-                value={searchText}
-                onChange={(e) => {
-                  setSearchText(e.target.value);
-                  performSearch(e.target.value);
-                }}
-              />
+              <div className="flex flex-row items-center">
+                <input
+                  type="text"
+                  placeholder="جستجو"
+                  className="formInput2 ml-8"
+                  value={searchText}
+                  onChange={(e) => {
+                    setSearchText(e.target.value);
+                    performSearch(e.target.value);
+                  }}
+                />
+                <div className="flex items-center gap-3 w-1/3">
+                  <label
+                    htmlFor="startDate"
+                    className="text-[#767676] font-bold"
+                  >
+                    شروع:
+                  </label>
+
+                  <DatePicker
+                    inputClass="formInput text-black bg-gray-200 w-2/3"
+                    format="YYYY-MM-DD"
+                    calendar={persian}
+                    locale={persian_fa}
+                    calendarPosition="bottom-right"
+                    onChange={(e: DateObject) => setStartDate(e.unix)}
+                  />
+                </div>
+                <div className="flex items-center gap-3 w-1/3">
+                  <label htmlFor="endDate" className="text-[#767676] font-bold">
+                    پایان:
+                  </label>
+
+                  <DatePicker
+                    inputClass="formInput text-black bg-gray-200 w-2/3"
+                    format="YYYY-MM-DD"
+                    calendar={persian}
+                    locale={persian_fa}
+                    calendarPosition="bottom-right"
+                    onChange={(e: DateObject) => setEndDate(e.unix)}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-row justify-between items-center mt-7">
+                <button
+                  type="button"
+                  onClick={() => setEmpty(!empty)}
+                  className={`hover:bg-fuchsia-800 dark:hover:bg-fuchsia-800 bg-fuchsia dark:bg-fuchsia-900 bg-opacity-50 text-black font-semibold dark:text-white py-2 px-14 border border-gray-400 rounded shadow ${
+                    empty
+                      ? "dark:bg-gray-500 bg-gray-500 hover:bg-gray-400 dark:hover:bg-gray-400"
+                      : ""
+                  }`}
+                >
+                  سازه های خالی
+                </button>
+                <h3 className="py-2 px-3"> {mainData.length}</h3>
+              </div>
               <div className="mt-4 bg-cyan-100 text-gray-700 font-bold rounded-xl p-3 h-[200px] overflow-y-auto">
                 <ul>
                   {searchResults.length === 0
-                    ? data.map((item, index) => {
+                    ? mainData.map((item, index) => {
                         return (
                           <li
                             className="mt-2 cursor-pointer hover:text-red-700 transition-all"
